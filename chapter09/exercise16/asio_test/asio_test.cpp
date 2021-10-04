eefea8be3be9882a2c7088c48c14549f52f362af
diodoros87 <diodoros1987@gmail.com>
2021-10-04 09:03:31 +0200

    ex 16 ch 9

--- chapter09/exercise16/asio_test/asio_test.cpp
+++ chapter09/exercise16/asio_test/asio_test.cpp
@@ -4,8 +4,10 @@
 #include <sstream>
 #include <utility>
 #include <string>
-#include <map>      
+#include <map>    
 #include <array> 
+//#define NDEBUG
+#include <cassert>
 
 #include "nlohmann/json.hpp"
 
@@ -14,13 +16,23 @@ using namespace std;
 enum class Cache_control { no_store, no_cache };
 inline ostream& operator<<(ostream& os, const Cache_control& x) {
    static const array<string, 2> AVAILABLE_CACHE_CONTROLS = { "no-store", "no-cache" };
-   return os << AVAILABLE_CACHE_CONTROLS[static_cast<int>(x)];
+   const int INDEX = static_cast<int>(x);
+   if (INDEX < 0 || INDEX >= AVAILABLE_CACHE_CONTROLS.size()){
+      assert(false);
+      return os << AVAILABLE_CACHE_CONTROLS[0];
+   }
+   return os << AVAILABLE_CACHE_CONTROLS[INDEX];
 }
 
 enum class Connection { keep_alive, close };
 inline ostream& operator<<(ostream& os, const Connection& x) {
    static const array<string, 2> AVAILABLE_CONNECTIONS = { "keep-alive", "close" };
-   return os << AVAILABLE_CONNECTIONS[static_cast<int>(x)];
+   const int INDEX = static_cast<int>(x);
+   if (INDEX < 0 || INDEX >= AVAILABLE_CONNECTIONS.size()) {
+      assert(false);
+      return os << AVAILABLE_CONNECTIONS[0];
+   }
+   return os << AVAILABLE_CONNECTIONS[INDEX];
 }
 
 void process_json_document(const string & CURRENCY, const string & DOC);
@@ -28,7 +40,7 @@ string get_json_document(const string & HOST, const string & DIRECTORY, const Ca
 
 int main() {
    try {
-      const string CURRENCY = "USD";
+      const string CURRENCY = "PLN";
       const string HOST = "www.floatrates.com";
       const string DIRECTORY = "/daily/" + CURRENCY + ".json";
       const Connection CONNECTION = Connection::close;
@@ -65,21 +77,21 @@ void from_json(const nlohmann::json& jdata, float_rates_info& info) {
    info.inverse_rate = jdata.at("inverseRate").get<double>();
 }
 
-void prepare_json_document(string & doc);
+void modify_downloaded_document(string & doc);
 
 string get_json_document(const string & HOST, const string & DIRECTORY, const Cache_control & CACHE_CONTROL, const Connection & CONNECTION) {
    asio::ip::tcp::iostream stream;
    stream.connect(HOST, "http");
-   stream    << "GET " << DIRECTORY << " HTTP/1.1\r\n";
+   stream    << "GET " << DIRECTORY << " HTTP/2.0\r\n";
    stream    << "Host: " + HOST + "\r\n";
-   stream    << "Accept: application/json\r\n";
+   stream    << "Accept: application/json";
    stream    << "Cache-Control: " << CACHE_CONTROL << "\r\n";
-   stream    << "Connection: " << CONNECTION << "\r\n\r\n" << flush;
+   stream    << "Connection: " << CONNECTION  << "\r\n\r\n";
 
    ostringstream os;
    os << stream.rdbuf();
    string result = os.str();
-   prepare_json_document(result);
+   modify_downloaded_document(result);
    return result;
 }
 
@@ -113,7 +125,7 @@ void erase(string & result, const char C, const char A) {
    }
 }
 
-void prepare_json_document(string & doc) {
+void modify_downloaded_document(string & doc) {
    size_t first = doc.find("{");
    doc = doc.substr(first);
    size_t last = doc.rfind("}");
