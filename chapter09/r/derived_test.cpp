6b8e0e81647ce141887301df41530cda354ebd55
diodoros87 <diodoros1987@gmail.com>
2021-12-20 17:20:11 +0100

    t

--- chapter09/r/derived_test.cpp
+++ chapter09/r/derived_test.cpp
@@ -33,6 +33,38 @@ Result_codes incorrect_call(Object & object, Func_1 && m_funct,
    return result;
 }
 
+static Result_codes test_derived_linking(Derived & d) {
+   print_and_assert(d.X(), 4.0, "x", __func__);
+   print_and_assert(d.pv_Y(), 101.0, "y", __func__);
+   print_and_assert(d.Z(), 7.0, "z", __func__);
+   print_and_assert(d.pv_char(), Derived::DERIVED_CHAR, "DERIVED_CHAR", __func__);
+   print_and_assert(d.number(), Derived::DERIVED, "number", __func__);
+
+   Result_codes result = bind_execute_member_function_assert(d, &Derived::X, 7.5, "x", __func__, &Derived::virt_set_X, 7.5);
+   if (OK == result)
+      result = incorrect_call(d, &Derived::X, numeric_limits<double>::infinity(), "x", __func__, &Derived::virt_set_X, numeric_limits<double>::infinity());
+   if (OK == result) {
+      print_and_assert(d.X(), 7.5, "x", __func__);
+      result = bind_execute_member_function_assert(d, &Derived::pv_Y, 0.8, "y", __func__, &Derived::virt_set_Y, 0.8);
+   }
+   if (OK == result)
+         result = incorrect_call(d, &Derived::pv_Y, -105.8, "y", __func__, &Derived::virt_set_Y, -105.8); 
+   if (OK == result) {
+      print_and_assert(d.pv_Y(), 0.8, "y", __func__);
+      result = bind_execute_member_function_assert(d, &Derived::Z, -66.8, "z", __func__, &Derived::virt_set_Z, -66.8);
+   }
+   if (OK == result)
+         result = incorrect_call(d, &Derived::Z, -0.0, "z", __func__, &Derived::virt_set_Z, -0.0); 
+   if (OK == result)
+         result = incorrect_call(d, &Derived::Z, 190.0, "z", __func__, &Derived::virt_set_Z, 190.0);
+   if (OK == result) {
+      print_and_assert(d.Z(), -66.8, "z", __func__);
+      print_and_assert(d.virt_area(), 7.5 * 0.8 * -66.8, "virt_area", __func__);
+   }
+   assert_many(result == OK, "result == ", result);
+   return result;
+}
+
 #ifdef MANUAL_DLL_LOAD
 typedef struct Manual_DLL_interface {
    Derived * (*create)(const double, const double, const double);
@@ -56,6 +88,25 @@ static Derived * test_constructor(Derived * (*create)(const double, const double
    return d;
 }
 
+static Result_codes test_derived_linking() {
+   cerr << "\nMANUAL DLL LOAD\n";
+   Manual_DLL_interface derived_functions;
+   load_derived(derived_functions);
+   Derived * d = test_constructor(derived_functions.create);
+   Result_codes result = (nullptr == d) ? INCORRECT_VALUE : OK;
+   if (result == OK) {
+         result = test_derived_linking(*d);
+      if (result == OK)
+         result = static_cast<Result_codes> (close_handle(&(derived_functions.handle)));
+      else
+         close_handle(&(derived_functions.handle));
+      derived_functions.destroy(d);
+   }
+   assert_many(result == OK, "result == ", result);
+   assert_many(d == nullptr, "d pointer == ", d);
+   return result;
+}
+/*
 static Result_codes test_derived_linking() {
    cerr << "\nMANUAL DLL LOAD\n";
    Manual_DLL_interface derived_functions;
@@ -94,6 +145,7 @@ static Result_codes test_derived_linking() {
    assert_many(d == nullptr, "d pointer == ", d);
    return result;
 }
+*/
 #else
 static Result_codes test_constructor(Derived & d) {
    Result_codes result = init<Derived>(d, Constructor<Derived>(), 4, -3, 7); 
@@ -105,6 +157,17 @@ static Result_codes test_constructor(Derived & d) {
    return result;
 }
 
+static Result_codes test_derived_linking() {
+   cerr << "\nAUTOMATIC DLL LOAD\n";
+   Derived d;
+   Result_codes result = test_constructor(d);
+   assert_many(result == OK, "result == ", result);
+   if (result == OK)
+      result = test_derived_linking(d);
+   assert_many(result == OK, "result == ", result);
+   return result;
+}
+/*
 static Result_codes test_derived_linking() {
    cerr << "\nAUTOMATIC DLL LOAD\n";
    Derived d;
@@ -143,10 +206,11 @@ static Result_codes test_derived_linking() {
    assert_many(result == OK, "result == ", result);
    return result;
 }
+*/
 #endif
 
 Result_codes test_derived() {
-   function<Result_codes()> test = bind(&test_derived_linking);
+   function<Result_codes()> test = bind(static_cast<Result_codes(*)()> (&test_derived_linking));
    Result_codes result = call_catch_exception(test);
    return result; 
 }
